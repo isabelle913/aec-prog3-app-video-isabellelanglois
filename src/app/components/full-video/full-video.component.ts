33829ca48c46e47bb93ca71425f84d8cdde94368
isabelle913 <atelierisabelle@videotron.ca>
2023-10-21 09:10:30 -0400

    effacer fichier shared-style

--- src/app/components/full-video/full-video.component.ts
+++ src/app/components/full-video/full-video.component.ts
@@ -4,7 +4,7 @@ import { IVideo } from 'src/app/interfaces/ivideo';
 @Component({
   selector: 'app-full-video',
   templateUrl: './full-video.component.html',
-  styleUrls: ['../../shared/shared-style.css', './full-video.component.css'],
+  styleUrls: ['./full-video.component.css'],
 })
 export class FullVideoComponent {
   @Input() video?: IVideo;
--- src/app/components/liste-video/liste-video.component.ts
+++ src/app/components/liste-video/liste-video.component.ts
@@ -4,7 +4,7 @@ import { VIDEOS } from 'src/app/mocks/mock-videos';
 @Component({
   selector: 'app-liste-video',
   templateUrl: './liste-video.component.html',
-  styleUrls: ['../../shared/shared-style.css', './liste-video.component.css'],
+  styleUrls: ['./liste-video.component.css'],
 })
 export class ListeVideoComponent {
   videos = VIDEOS;
