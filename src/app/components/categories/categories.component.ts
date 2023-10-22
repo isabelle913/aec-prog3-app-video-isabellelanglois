c0e90d4be6f639a6d3ba4a4acb4ebb1fa86d3670
isabelle913 <atelierisabelle@videotron.ca>
2023-10-22 17:59:05 -0400

    Modifier comportement component categories

--- src/app/components/categories/categories.component.ts
+++ src/app/components/categories/categories.component.ts
@@ -7,4 +7,14 @@ import { Component, Input } from '@angular/core';
 })
 export class CategoriesComponent {
   @Input() categories: string[] = [];
+  @Input() stacked: boolean = false;
+
+  randomColor() {
+    const indexRandom = Math.trunc(Math.random() * 3) + 1;
+    console.log(indexRandom);
+    return this.availableColors[indexRandom];
+  }
+
+  availableColors: string[] = ['primary', 'accent', 'warn'];
 }
+// TODO ?? voir erreur dans console
