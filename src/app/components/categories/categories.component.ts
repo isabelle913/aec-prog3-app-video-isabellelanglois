145f271b713aa9839bbdd825e9fb4fafdd773d24
isabelle913 <atelierisabelle@videotron.ca>
2023-10-24 05:51:23 -0400

    comportement des couleurs des chips

--- src/app/components/categories/categories.component.ts
+++ src/app/components/categories/categories.component.ts
@@ -8,13 +8,28 @@ import { Component, Input } from '@angular/core';
 export class CategoriesComponent {
   @Input() categories: string[] = [];
   @Input() stacked: boolean = false;
+  @Input() color: boolean = false;
+
+  colorPosition = -1;
+  availableColors: string[] = ['primary', 'accent', 'warn'];
 
   randomColor() {
+    if (this.color === false) return;
     const indexRandom = Math.trunc(Math.random() * 3) + 1;
-    console.log(indexRandom);
+    console.log('indexRandom', indexRandom);
     return this.availableColors[indexRandom];
   }
 
-  availableColors: string[] = ['primary', 'accent', 'warn'];
+  getColor() {
+    if (this.colorPosition > this.availableColors.length - 1) {
+      this.colorPosition = -1;
+    }
+    this.colorPosition++;
+    console.log('colorPosition', this.colorPosition);
+    return this.availableColors[this.colorPosition];
+  }
+  ngOnInit() {
+    this.colorPosition = -1;
+  }
 }
 // TODO ?? voir erreur dans console
