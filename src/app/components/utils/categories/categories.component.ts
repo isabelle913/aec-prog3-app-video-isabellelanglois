7b6c58c46eb016841d433c31734d087c5c6a65f5
isabelle913 <atelierisabelle@videotron.ca>
2023-10-31 19:21:35 -0400

    Correctes error to use random color

--- src/app/components/utils/categories/categories.component.ts
+++ src/app/components/utils/categories/categories.component.ts
@@ -1,35 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CategoriesComponent {
   @Input() categories: string[] | undefined = [];
   @Input() stacked: boolean = false;
   @Input() color: boolean = false;
 
-  colorPosition = -1;
   availableColors: string[] = ['primary', 'accent', 'warn'];
 
   randomColor() {
     if (this.color === false) return;
     const indexRandom = Math.trunc(Math.random() * 3) + 1;
-    console.log('indexRandom', indexRandom);
     return this.availableColors[indexRandom];
   }
-
-  getColor() {
-    if (this.colorPosition > this.availableColors.length - 1) {
-      this.colorPosition = -1;
-    }
-    this.colorPosition++;
-    console.log('colorPosition', this.colorPosition);
-    return this.availableColors[this.colorPosition];
-  }
-  ngOnInit() {
-    this.colorPosition = -1;
-  }
 }
-// TODO ?? voir erreur dans console les couleurs données aux chips
