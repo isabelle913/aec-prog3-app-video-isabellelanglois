6dc5f89977635bd8c5b0093c4c86239ebd502cd8
isabelle913 <atelierisabelle@videotron.ca>
2023-10-31 19:27:21 -0400

    avanced small video

--- src/app/components/small-video/small-video.component.ts
+++ src/app/components/small-video/small-video.component.ts
@@ -10,6 +10,41 @@ import { IVideo } from 'src/app/interfaces/ivideo';
 export class SmallVideoComponent {
   @Input() video?: IVideo;
   mediaDefaut = '../../../assets/logo_300-170.png';
+  imgCitron =
+    'https://www.arnaques-infos.org/images/pixabay-bb4f3cce9a2f4650_l7y9x3p4.png';
+  imgStar = '../../../assets/star-70x72.png';
+  // isHidden =
+  //   this.video?.score && this.video?.score > -4999 && this.video?.score < 4999
+  //     ? true
+  //     : false;
+  // isHidden = this.video?.score ? true : false;
+  // isHidden = true;
+
+  // isStarActive = this.video?.score
+  //   ? this.video?.score > 4999
+  //     ? true
+  //     : false
+  //   : false;
+  // isStarActive = this.video?.score ? true : false; => ca donne false
+
+  // isCitrusActive = this.video?.score
+  //   ? this.video?.score < -4999
+  //     ? true
+  //     : false
+  //   : false;
+  // TODO ?? gérer  les condition avec variable possiblement undefined
+  isStarActive() {
+    if (this.video?.score) {
+      return this.video.score > 4999 ? true : false;
+    }
+    return false;
+  }
+  isCitrusActive() {
+    if (this.video?.score) {
+      return this.video.score < -4999 ? true : false;
+    }
+    return false;
+  }
 
   constructor(private router: Router) {}
 
