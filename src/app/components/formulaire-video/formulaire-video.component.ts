8f499bc55f2157c471ad62a42382b0b11c5c72cf
isabelle913 <atelierisabelle@videotron.ca>
2023-11-07 22:17:20 -0500

    creation component formulaire ajout vidéo

---
+++ src/app/components/formulaire-video/formulaire-video.component.ts
@@ -0,0 +1,38 @@
+import { Component, inject } from '@angular/core';
+import { IVideo } from 'src/app/interfaces/ivideo';
+import { AUTEURS } from 'src/app/mocks/mock-auteurs';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+
+@Component({
+  selector: 'app-formulaire-video',
+  templateUrl: './formulaire-video.component.html',
+  styleUrls: ['./formulaire-video.component.css'],
+})
+export class FormulaireVideoComponent {
+  newVideo: IVideo = {
+    nom: '',
+    description: '',
+    media: '',
+    code: '',
+    categories: ['allo', 'Coucou'],
+    auteur: AUTEURS[0],
+    datePublication: '',
+    duree: 0,
+    nbVues: 0,
+    score: 0,
+    subtitle: '',
+    avis: [],
+  };
+
+  announcer = inject(LiveAnnouncer);
+
+  remove(category: string): void {
+    const index = this.newVideo.categories.indexOf(category);
+
+    if (index >= 0) {
+      this.newVideo.categories.splice(index, 1);
+
+      this.announcer.announce(`Removed ${category}`);
+    }
+  }
+}
