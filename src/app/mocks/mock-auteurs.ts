3a20045b0a6237537e51b1dd0594abe60179368b
isabelle913 <atelierisabelle@videotron.ca>
2023-11-07 22:19:11 -0500

    création mock auteur

---
+++ src/app/mocks/mock-auteurs.ts
@@ -0,0 +1,12 @@
+import { IAuteur } from '../interfaces/iauteur';
+
+export const AUTEURS: IAuteur[] = [
+  {
+    nom: 'Jeanne BlancheVille',
+    utilisateur: 'Jeanne',
+    verifie: false,
+    avatar: '',
+    coordonnees: {},
+    auteurDescription: 'Une description',
+  },
+];
