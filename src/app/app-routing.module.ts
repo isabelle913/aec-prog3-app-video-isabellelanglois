e0afdfa23975e62ca60136c14390addbefb03638
isabelle913 <atelierisabelle@videotron.ca>
2023-12-29 06:51:42 -0500

    Ajouter route pour les favoris

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,10 @@ import { FullVideoComponent } from './components/full-video/full-video.component
 import { ListeVideoComponent } from './components/liste-video/liste-video.component';
 import { FormulaireVideoComponent } from './components/formulaire-video/formulaire-video.component';
 
+// TODO corriger pour router avec idvideo et non code
 const routes: Routes = [
   { path: 'videos', component: ListeVideoComponent },
+  { path: 'videos/favorite', component: ListeVideoComponent },
   { path: 'video/:code', component: FullVideoComponent },
   { path: 'add', component: FormulaireVideoComponent },
   { path: '', redirectTo: '/videos', pathMatch: 'full' },
