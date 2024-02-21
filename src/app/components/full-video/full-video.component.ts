7ef08d05b08410da75fea18388aade88c19811d0
isabelle913 <atelierisabelle@videotron.ca>
2024-02-21 05:23:13 -0500

    added guard if not avis in getavis

--- src/app/components/full-video/full-video.component.ts
+++ src/app/components/full-video/full-video.component.ts
@@ -3,8 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { IVideo } from 'src/app/interfaces/ivideo';
-import { VIDEOS } from 'src/app/mocks/mock-videos';
-import { VideoService } from 'src/app/services/video.service';
+import { VideoService } from 'src/app/services/video/video.service';
 import { AvisService } from 'src/app/services/avis/avis.service';
 
 @Component({
@@ -38,18 +37,13 @@ export class FullVideoComponent {
     this.videoService.getOneVideo(id).subscribe((response) => {
       if (!response) return;
       this.video = response;
-      console.log('Video', this.video);
       this.getAvisOneVideo(this.video.id);
     });
   }
   getAvisOneVideo(id_video: number) {
     this.avisService.getAvisOneVideo(id_video).subscribe((response) => {
-      console.log(response);
+      if (typeof response === 'string') return;
       if (response && this.video) this.video.avis = response;
-      console.log('Video', this.video);
     });
   }
 }
-// TODO corrigé si pas d'avis erreur dans la console
-
-// <!-- TODO idem pour le score soit celui reçu du serveur soit utilisé -->
