6afedabe4858e6b11966414173a5b176a0503191
isabelle913 <atelierisabelle@videotron.ca>
2024-02-21 05:25:31 -0500

    score use

--- src/app/components/utils/thumbs/thumbs.component.ts
+++ src/app/components/utils/thumbs/thumbs.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { ScoreService } from 'src/app/services/score/score.service';
 
 @Component({
   selector: 'app-thumbs',
@@ -7,12 +8,26 @@ import { Component, Input } from '@angular/core';
 })
 export class ThumbsComponent {
   @Input() score = 0;
+  @Input() id_video = 0;
+  objScore = { score: 0 };
+
+  constructor(private scoreService: ScoreService) {}
+
+  getScore() {
+    this.scoreService.getScore(this.id_video).subscribe((response) => {
+      this.score = response.score;
+    });
+  }
 
   increase() {
-    this.score++;
+    this.scoreService.updateScore(this.id_video, 'add').subscribe(() => {
+      this.getScore();
+    });
   }
 
   decrease() {
-    this.score--;
+    this.scoreService.updateScore(this.id_video, 'lower').subscribe(() => {
+      this.getScore();
+    });
   }
 }
---
+++ src/app/interfaces/iscore.ts
@@ -0,0 +1,3 @@
+export interface Iscore {
+  score: number;
+}
