e33134e1a242f74970397b6d492121d315bfcd19
isabelle913 <atelierisabelle@videotron.ca>
2024-02-23 20:18:26 -0500

    added btn to add avis in full video component

--- src/app/components/full-video/full-video.component.ts
+++ src/app/components/full-video/full-video.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { IVideo } from 'src/app/interfaces/ivideo';
 import { VideoService } from 'src/app/services/video/video.service';
 import { AvisService } from 'src/app/services/avis/avis.service';
-
-// TODO mettre dans le full video une place pour ajouter un avis
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { FormulaireAvisComponent } from '../formulaire-avis/formulaire-avis.component';
+import { IAvis } from 'src/app/interfaces/iavis';
 
 @Component({
   selector: 'app-full-video',
@@ -16,12 +18,20 @@ import { AvisService } from 'src/app/services/avis/avis.service';
 export class FullVideoComponent {
   video: IVideo | undefined;
   mediaDefaut = '../../../assets/logo_300-170.png';
+  avis: IAvis = {
+    id: 0,
+    id_video: 0,
+    note: 0,
+    commentaire: '',
+  };
 
   constructor(
     private route: ActivatedRoute,
     private location: Location,
     private videoService: VideoService,
-    private avisService: AvisService
+    private avisService: AvisService,
+    private dialog: MatDialog,
+    private _snackBar: MatSnackBar
   ) {}
 
   ngOnInit(): void {
@@ -35,6 +45,7 @@ export class FullVideoComponent {
     const id: number | null = Number(this.route.snapshot.paramMap.get('id'));
 
     if (!id) return;
+    this.avis.id_video = id;
 
     this.videoService.getOneVideo(id).subscribe((response) => {
       if (!response) return;
@@ -46,6 +57,18 @@ export class FullVideoComponent {
     this.avisService.getAvisOneVideo(id_video).subscribe((response) => {
       if (typeof response === 'string') return;
       if (response && this.video) this.video.avis = response;
+    });
+  }
+  openFormAvis() {
+    const dialogRef = this.dialog.open(FormulaireAvisComponent, {
+      data: this.avis,
+    });
+
+    dialogRef.afterClosed().subscribe((response) => {
+      if (response) {
+        this._snackBar.open(response, undefined, { duration: 2000 });
+        if (this.video) this.getAvisOneVideo(this.video.id);
+      }
     });
   }
 }
